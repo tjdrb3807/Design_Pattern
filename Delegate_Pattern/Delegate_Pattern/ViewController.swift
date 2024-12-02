33459df5f171635967e208450ae636cf03698e54
Jeon_Seong_gyu <junsung3807@naver.com>
2024-12-02 12:57:06 +0900

    🎉 Create delegate pattern project

---
+++ Delegate_Pattern/Delegate_Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delegate_Pattern
+//
+//  Created by 전성규 on 12/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
